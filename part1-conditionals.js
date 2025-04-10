189f2004f7be3e225ba16a528e4da42644abed69
TutCode222 <tuddycarraway@gmail.com>
2025-04-09 22:15:44 -0500

    Added code for first question

---
+++ part1-conditionals.js
@@ -0,0 +1,34 @@
+// Grade Checker
+// Print A, B, C, D, or F based on score
+
+let score = 82;
+
+if (score >= 90) {
+    console.log("A");
+} else if (score >= 80) {
+    console.log("B");
+} else if (score >= 70) {
+    console.log("C");
+} else if (score >= 60) {
+    console.log("D");
+} else {
+    console.log("F");
+}
+
+
+
+
+// Pizza Quantity Warning
+// Print warning if quantity > 10
+
+let quantity = 12;
+
+if (quantity > 10) {
+    console.log("Warning: Too many pizzas!");
+}
+
+
+// Login Simulator
+
+let password = "letmein"; // or use prompt
+// Print "Access granted" or "Access denied"
