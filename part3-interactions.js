0c8a7b7b6a5ece5ae67633591eabd59e0d724759
TutCode222 <tuddycarraway@gmail.com>
2025-04-11 19:56:28 -0500

    completed function for name input and submit button on part3

--- part3-interactions.js
+++ part3-interactions.js
@@ -4,3 +4,12 @@
 function sayHello() {
     document.getElementById("message").textContent = "Hello there!";
   }
+
+
+  // Complete function
+  // Show Name with input placeholder and submit button
+
+  function showName() {
+    const name = document.getElementById("nameInput").value;
+    document.getElementById("output").textContent = `Hello, ${name}!`;
+  }
