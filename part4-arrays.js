f74f6b3b88254eea1a9aa780e762446d7724d96c
TutCode222 <tuddycarraway@gmail.com>
2025-04-16 20:13:50 -0500

    Added answers to problem 1

---
+++ part4-arrays.js
@@ -0,0 +1,14 @@
+// PROBLEM #1
+// 1. Create an array with 5 of your favorite foods
+let favoriteFoods = ['fruit', 'Cajun food', 'Soul food', 'pasta', 'soups'];
+
+// 2. Log the first and last food in the array
+console.log('First food:', favoriteFoods[0]);
+console.log('Last food:', favoriteFoods[favoriteFoods.length - 1]);
+
+// 3. Add one more food using .push() and log the new array
+favoriteFoods.push('Mexican');
+console.log('Updated favorite foods:', favoriteFoods);
+
+
+// PROBLEM #2
